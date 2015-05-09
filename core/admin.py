182c0d849b743ea645b91bfed8e3a838bb3762b1
dhilipsiva <dhilipsiva@gmail.com>
2015-05-09 10:53:40 +0530

    Registering the models for admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from core.models import User, Server, Account, Message, PrivateMessage
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Server)
+admin.site.register(Account)
+admin.site.register(Message)
+admin.site.register(PrivateMessage)
