3933bc55c03112f30dd965d85fdd1e3b4e2e145f
dhilipsiva <dhilipsiva@gmail.com>
2015-05-10 12:06:36 +0530

    Probably some stupid stuff

--- core/migrations/0001_initial.py
+++ core/migrations/0001_initial.py
@@ -19,7 +19,6 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='User',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('password', models.CharField(max_length=128, verbose_name='password')),
                 ('last_login', models.DateTimeField(null=True, verbose_name='last login', blank=True)),
                 ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
@@ -30,8 +29,8 @@ class Migration(migrations.Migration):
                 ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
                 ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
                 ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
-                ('uuid', models.UUIDField(default=uuid.uuid4, editable=False)),
-                ('socket_uuid', models.UUIDField(default=uuid.uuid4, editable=False)),
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
+                ('socket', models.UUIDField(default=uuid.uuid4, editable=False)),
                 ('groups', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups')),
                 ('user_permissions', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions')),
             ],
@@ -45,24 +44,25 @@ class Migration(migrations.Migration):
             ],
         ),
         migrations.CreateModel(
-            name='Account',
+            name='Channel',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('nickname', models.CharField(max_length=256)),
-                ('username', models.CharField(max_length=256)),
-                ('password', models.CharField(max_length=256)),
-                ('port', models.PositiveIntegerField(default=6667, blank=True)),
-                ('uuid', models.UUIDField(default=uuid.uuid4, editable=False)),
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
+                ('name', models.CharField(max_length=256)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Conversation',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
             ],
         ),
         migrations.CreateModel(
             name='Message',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
                 ('text', models.TextField()),
-                ('uuid', models.UUIDField(default=uuid.uuid4, editable=False)),
                 ('created_on', models.DateTimeField(auto_now_add=True)),
-                ('from_account', models.ForeignKey(related_name='messages_sent', to='core.Account')),
+                ('channel', models.ForeignKey(related_name='messages', to='core.Channel')),
             ],
             options={
                 'abstract': False,
@@ -71,12 +71,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='PrivateMessage',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
                 ('text', models.TextField()),
-                ('uuid', models.UUIDField(default=uuid.uuid4, editable=False)),
                 ('created_on', models.DateTimeField(auto_now_add=True)),
-                ('from_account', models.ForeignKey(related_name='private_messages_sent', to='core.Account')),
-                ('to_account', models.ForeignKey(related_name='private_messages_received', to='core.Account')),
+                ('read', models.BooleanField(default=False)),
+                ('conversation', models.ForeignKey(related_name='private_messages', to='core.Conversation')),
             ],
             options={
                 'abstract': False,
@@ -85,24 +84,64 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Server',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('hostname', models.CharField(max_length=256)),
-                ('uuid', models.UUIDField(default=uuid.uuid4, editable=False)),
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
+                ('host', models.CharField(max_length=256)),
+                ('port', models.PositiveIntegerField(default=6667, blank=True)),
+                ('is_ssl', models.BooleanField(default=False)),
+                ('is_sasl', models.BooleanField(default=False)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='UserChannel',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
+                ('nickname', models.CharField(max_length=256)),
+                ('password', models.CharField(max_length=256, null=True, blank=True)),
+                ('mode', models.CharField(max_length=16, null=True, blank=True)),
+                ('channel', models.ForeignKey(related_name='user_channels', to='core.Channel')),
+            ],
+        ),
+        migrations.CreateModel(
+            name='UserServer',
+            fields=[
+                ('id', models.UUIDField(default=uuid.uuid4, serialize=False, editable=False, primary_key=True)),
+                ('label', models.CharField(default=b'My IRC Server', max_length=256)),
+                ('username', models.CharField(max_length=256)),
+                ('password', models.CharField(max_length=256, null=True, blank=True)),
+                ('nickname', models.CharField(max_length=256)),
+                ('realname', models.CharField(max_length=256, null=True, blank=True)),
+                ('server', models.ForeignKey(related_name='user_servers', to='core.Server')),
+                ('user', models.ForeignKey(related_name='user_servers', to=settings.AUTH_USER_MODEL)),
             ],
         ),
         migrations.AddField(
+            model_name='userchannel',
+            name='user_server',
+            field=models.ForeignKey(related_name='user_channels', to='core.UserServer', null=True),
+        ),
+        migrations.AddField(
+            model_name='privatemessage',
+            name='user_channel',
+            field=models.ForeignKey(related_name='private_messages', to='core.UserChannel'),
+        ),
+        migrations.AddField(
             model_name='message',
-            name='to_server',
-            field=models.ForeignKey(related_name='messages_received', to='core.Server'),
+            name='user_channel',
+            field=models.ForeignKey(related_name='messages', to='core.UserChannel'),
         ),
         migrations.AddField(
-            model_name='account',
+            model_name='conversation',
+            name='user_channel_1',
+            field=models.ForeignKey(related_name='+', to='core.UserChannel'),
+        ),
+        migrations.AddField(
+            model_name='conversation',
+            name='user_channel_2',
+            field=models.ForeignKey(related_name='+', to='core.UserChannel'),
+        ),
+        migrations.AddField(
+            model_name='channel',
             name='server',
-            field=models.ForeignKey(related_name='accounts', to='core.Server'),
-        ),
-        migrations.AddField(
-            model_name='account',
-            name='user',
-            field=models.ForeignKey(related_name='accounts', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(related_name='channels', to='core.Server'),
         ),
     ]
